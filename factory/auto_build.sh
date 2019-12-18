177d0cf203aef642b159de9c4c1a96808fecb461
nitsoh <22869526+nitsoh@users.noreply.github.com>
2019-12-18 10:21:51 +0800

    修改auto_build.sh

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -7,11 +7,12 @@ git fetch --all
 git reset --hard origin/master
 
 cd factory
-python3 ad.py
+# python3 ad.py
 python3 gfwlist.py
 python3 build_confs.py
 cd ..
 
 git add .
-git commit -m "Nightly build" -m "已合并最新的去广告规则及 GFWList"
+git commit -m "Nightly build"
 git push
+
